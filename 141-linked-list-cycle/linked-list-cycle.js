6b079fac6a59780908748c6474a82d450e8113be
Mayank Pandey <134437641+pandeymynk@users.noreply.github.com>
2025-12-07 18:18:40 +0530

    Time: 61 ms (34.71%) | Memory: 59.2 MB (68.55%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,14 +11,18 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let newNodes = new Set();
-    let curr = head;
-    while(curr != null){
-        if(newNodes.has(curr)){
-            return true;
-        }
-        newNodes.add(curr);
-        curr = curr.next;
+  if(!head) return false;
+  
+  let slow = head;
+  let fast = head.next;
+
+  while(slow != fast ){
+    if(fast === null || fast.next === null){
+        return false;
     }
-    return false;
+    slow = slow.next;
+    fast = fast.next.next;
+    
+  }
+  return true;
 };
