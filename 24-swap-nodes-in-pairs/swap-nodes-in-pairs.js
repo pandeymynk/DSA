4860ebcda6162fc091cfa26338080dcae27ec1b2
Mayank Pandey <134437641+pandeymynk@users.noreply.github.com>
2025-12-16 22:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (6.75%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+
+    let dummy = new ListNode();
+    let p = dummy;
+    let c = head;
+    let n = head.next;
+    dummy.next = head;
+    while(c && n){
+        p.next= n;
+        c.next = n.next
+        n.next = c;
+
+        p=c;
+        c=p.next;
+        n = c && c.next
+
+
+    }
+    return dummy.next;
+};
