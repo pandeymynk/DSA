b9376e453e87d802374750367e06997a17aa3b23
Mayank Pandey <134437641+pandeymynk@users.noreply.github.com>
2025-12-18 23:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (15.27%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -10,23 +10,33 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
+    // if(!head || !head.next) return head;
+
+    // let dummy = new ListNode();
+    // let p = dummy;
+    // let c = head;
+    // let n = head.next;
+    // dummy.next = head;
+    // while(c && n){
+    //     p.next= n;
+    //     c.next = n.next
+    //     n.next = c;
+
+    //     p=c;
+    //     c=p.next;
+    //     n = c && c.next
+
+
+    // }
+    // return dummy.next;
+
     if(!head || !head.next) return head;
 
-    let dummy = new ListNode();
-    let p = dummy;
-    let c = head;
-    let n = head.next;
-    dummy.next = head;
-    while(c && n){
-        p.next= n;
-        c.next = n.next
-        n.next = c;
+    let l = head;
+    let r = head.next;
+    
+    l.next = swapPairs(r.next);
+    r.next = l;
 
-        p=c;
-        c=p.next;
-        n = c && c.next
-
-
-    }
-    return dummy.next;
+    return r;
 };
