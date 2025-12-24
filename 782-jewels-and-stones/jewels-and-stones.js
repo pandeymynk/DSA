91f96715ef517483b8703df1013fe68de525da87
Mayank Pandey <134437641+pandeymynk@users.noreply.github.com>
2025-12-24 18:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (96.97%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+    for(let i =0;i<stones.length;i++){
+        for(let j = 0;j<jewels.length;j++){
+            if(jewels[j]=== stones[i]){
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+};
