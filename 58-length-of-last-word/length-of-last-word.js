0ee4c4d733c8eec637389047e087c08a65d3e74b
Mayank Pandey <134437641+pandeymynk@users.noreply.github.com>
2025-12-24 14:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (76.12%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s = s.trim();
-    s = s.split(" ");
-    return s[s.length-1].length;
+    // gO TO END 
+
+    let n = s.length-1; // start from last
+    let count=0;
+    while(n>=0){
+       if(s[n] != " "){
+        count++;
+       }
+       else if(count>0){
+        break;
+       }
+       n--;
+    }
+
+
+   
+
+    return count;
 };
