a0aa5326901f20ee38cd798136f00f5bd7ace24e
Mayank Pandey <134437641+pandeymynk@users.noreply.github.com>
2025-12-24 15:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (68.73%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -8,18 +8,16 @@ var findWordsContaining = function(words, x) {
     let s = words.length;
     let result = [];
     for(let i = 0; i<s; i++){
-        if(words[i].includes(x)){
-            result.push(i);
-        }
-        
-
-
-
-        // for(let j =0;i<s[i].length;j++){
-        //     if(s[j]===x){
-        //        result.push(i);
-        //     }
+        // if(words[i].includes(x)){
+        //     result.push(i);
         // }
+
+        for(let j =0;j<words[i].length;j++){
+            if(words[i][j]===x){
+               result.push(i);
+               break;
+            }
+        }
     }
     return result;
 };
